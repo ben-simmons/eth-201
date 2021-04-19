e064b61f542282ebf258f048743a541ac178de1a
Ben Simmons <simmons.a.ben@gmail.com>
2021-04-18 19:28:29 -0500

    Make my token ownable

--- token/contracts/token.sol
+++ token/contracts/token.sol
@@ -1,10 +1,11 @@
 pragma solidity 0.8.3;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, Ownable {
 
-    constructor() ERC20("MyToken", "MTKN") {
+    constructor() ERC20("Bencoin", "BEN") {
         _mint(msg.sender, 1000);
     }
 
